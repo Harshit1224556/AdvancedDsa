9e12b26ea0a6c0e910b5cfd0e15e14019532bc84
harshit <iamharshitsharma518@gmail.com>
2025-12-14 18:47:33 +0530

    better approach

--- recursion/palindromicstr.cpp
+++ recursion/palindromicstr.cpp
@@ -1,10 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool ispalindrome(string s,int st,int ed){
-      if(st>=ed) return true;
-      if(s[st]!=s[ed]) return false;
-       return ispalindrome(s,st+1,ed-1);
+// bool ispalindrome(string s,int st,int ed){
+//       if(st>=ed) return true;
+//       if(s[st]!=s[ed]) return false;
+//        return ispalindrome(s,st+1,ed-1);
+// }
+
+
+bool ispalindrome(int i,string s){
+    int n = s.length();
+    if(i>=(n/2)) return true;
+    if(s[i]!=s[n-i-1]) return false;
+    return ispalindrome(i+1,s);
 }
 int main()
 {
@@ -12,6 +20,6 @@ int main()
      string s;
      cin>>s;
      int n = s.length();
-     cout << ispalindrome(s,0,n-1);
+     cout << ispalindrome(0,s);
     return 0;
 }
