5039bd9855da6afac9ae255f6f257f3f3a8e7e36
harshit <iamharshitsharma518@gmail.com>
2026-01-20 19:04:13 +0530

    lets starts with the linkedlist today

---
+++ linkedlist/alloperation.java
@@ -0,0 +1,19 @@
+public class alloperation{
+    class Linkedlist{
+        static class node{
+            int val;
+            node next;
+            node(int val,node next){
+               this.val  = val;
+               this.next=next;
+            }
+        }
+           node head;
+        public void insertatthebeginning(int val){
+            
+        }
+    }
+    public static void main(String[] args) {
+       
+    }
+}
