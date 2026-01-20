f0a455e9c7c1757a986732c6f6515e68379c7972
harshit <iamharshitsharma518@gmail.com>
2026-01-20 21:29:47 +0530

    done with the linkedlist

--- linkedlist/alloperation.java
+++ linkedlist/alloperation.java
@@ -1,19 +1,55 @@
-public class alloperation{
-    class Linkedlist{
-        static class node{
+public class alloperation {
+
+    class Linkedlist {
+
+        public static class node {
             int val;
             node next;
-            node(int val,node next){
-               this.val  = val;
-               this.next=next;
+
+            node(int val) {
+                this.val = val;
+                
             }
         }
-           node head;
-        public void insertatthebeginning(int val){
+
+        node head = null;
+        node tail = null;
+        public void insertatthebeginning(int val) {
+            node temp = new node(val);
+            temp.next = head;
+            head=temp;
+        }
+        
+
+        public void insertattheend(int val)
+        {
+            node temp = new node(val);
+            node temps = head;
+            while(temps.next!=null){
+                temps=temps.next;
+            }
+            temps.next=temp;
+            temp=tail;
             
         }
+         
+        public void printlinkedlist() {
+            node temp = head;
+            while (temp != null) {
+                System.out.println(temp.val);
+                temp = temp.next;
+            }
+        }
     }
+
     public static void main(String[] args) {
-       
+        alloperation obj = new alloperation();
+        Linkedlist ll = obj.new Linkedlist();
+
+        ll.insertatthebeginning(10);
+        ll.insertatthebeginning(20);
+        ll.insertattheend(30);
+        ll.printlinkedlist();
+        
     }
 }
