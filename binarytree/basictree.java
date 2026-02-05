5e2078a29bb3875e581b87b45f1389a2df0b59e5
harshit <iamharshitsharma518@gmail.com>
2026-02-05 16:05:18 +0530

    created the tree

--- binarytree/basictree.java
+++ binarytree/basictree.java
@@ -1,7 +1,34 @@
 public class basictree{
+
+  public static class  Node{
+     int val;
+     Node left;
+     Node right;
+     Node(int val){
+        this.val = val;
+     }
+  }
+
     public static void main(String[] args) {
         
-        System.out.println("Hello world");
+        Node root = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        root.left = b;
+        root.right = c;
+
+        Node d = new Node(4);
+        Node e = new Node(5);
+        b.left = d;
+        b.right = e;
+
+
+        Node f = new Node(6);
+        Node g = new Node(7);
+        c.left = f;
+        c.right = g;
+    
+
 
     }
 }
